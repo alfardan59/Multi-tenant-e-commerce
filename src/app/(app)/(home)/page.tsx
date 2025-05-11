8d56b0ce6584e2956bb4bad2c388e40d67cde6f3
alfardan <alfardanarfin500@gmail.com>
2025-05-11 17:58:57 +0530

    feat: update Home component to fetch and display user data; add Categories collection configuration

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,31 +1,17 @@
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { Progress } from "@/components/ui/progress";
-import { Textarea } from "@/components/ui/textarea";
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
 
-export default function Home() {
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  })
+
+    const data = await payload.find({
+    collection: 'users',
+  })
   return (
-    <div className="p-4">
-    <div className="flex flex-col gap-y-4">
-      <div>
-        <Button variant="elevated">
-          I am a Button
-        </Button>
-      </div>
-      <div>
-        <Input  placeholder="I am an Input" />
-      </div>
-      <div>
-        <Progress value={50} />
-      </div>
-      <div>
-        <Textarea placeholder="I am a text area" />
-      </div>
-      <div>
-        <Checkbox />
-      </div>
-      </div>
+    <div>
+      {JSON.stringify(data,null,2)}
     </div>
   )
 }
