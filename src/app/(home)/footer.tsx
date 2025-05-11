bbf0b9980a900aa639d1ca23ad2f3f15525a7fae
alfardan <alfardanarfin500@gmail.com>
2025-05-11 14:10:40 +0530

    update layout structure and improve component styles for consistency

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <footer className="flex.border-this.justify-BetweenHorizonalEnd.font-MediaKeyStatusMap.p-6">
+            <div className="flex items-center gap-2">
+                <p>funroads pvt ltd.</p>
+            </div>
+        </footer>
+    )
+}
